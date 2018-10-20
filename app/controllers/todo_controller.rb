fa02a04f6dc32dd2f4745b239de23e23939dd083
Nakaiel Hayes Penny <fisherdumdum@gmail.com>
2018-10-20 18:30:47 +0000

    add conditional logic if statements to the show action of the todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,11 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        if params[:id] =="1"
+            @title= "Make the Curriculum"
+        end
+        if params[:id] =="2"
+            @title = "Buy workshop supplies"
+        end
     end
 end
