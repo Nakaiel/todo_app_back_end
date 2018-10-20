7053a73afa8f4a5c5a75feacb9005f7fd5ba6015
Nakaiel Hayes Penny <fisherdumdum@gmail.com>
2018-10-20 18:18:23 +0000

    add dynasty

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,6 @@ Rails.application.routes.draw do
   #   end
 get "add/:x/:y" => "application#add"
 get 'todo/index', to: 'todo#index'
-get 'todo/show', to: 'todo#show'
+get 'todo/show/:id', to: 'todo#show'
+
 end
